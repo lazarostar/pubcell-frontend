35a9c2267c4ad057032b25a550a826fddb5bebdd
lazarostar <nascimento.dev26@gmail.com>
2022-12-02 19:30:29 +0100

    feat: reload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import * as React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Button, CircularProgress, Box } from "@mui/material";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
+import { client } from "./utils";
 
 function App() {
+  const [reloading, setReloading] = React.useState(false);
+
+  const reload = async () => {
+    setReloading(true);
+
+    await client("reload");
+    setReloading(false);
+  };
+
   return (
     <div>
       <Link to="/">
@@ -12,6 +22,15 @@ function App() {
           Pubcell
         </Typography>
       </Link>
+      <Box sx={{ position: "absolute", right: 5, top: 5 }}>
+        {reloading ? (
+          <CircularProgress />
+        ) : (
+          <Button variant="contained" color="warning" onClick={reload}>
+            Reload
+          </Button>
+        )}
+      </Box>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:projectName" element={<Project />} />
